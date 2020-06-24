bc0c09df55e0a4759ea8ac029c75cfbc09acd761
itzmeowww <winthanasan@gmail.com>
2020-06-24 21:39:52 +0700

    Fix bug : text formatter

--- src/basic.js
+++ src/basic.js
@@ -7,6 +7,22 @@ const messageSenderGenerator = (message) => {
   };
 };
 
+const formatTxt = (txt, msg) => {
+  let message = msg;
+  let new_msg = "";
+  for (var i = 0; i < txt.length; i++) {
+    if (txt[i] === txt[i].toUpperCase()) {
+      new_msg += message.charAt(i).toUpperCase();
+    } else {
+      new_msg += message.charAt(i).toLowerCase();
+    }
+  }
+  for (var i = txt.length; i < message.length; i++) {
+    new_msg += message[i];
+  }
+  return new_msg;
+};
+
 const sendMessage = (message, bot, channel) => {
   bot.sendMessage({
     to: channel,
@@ -16,12 +32,12 @@ const sendMessage = (message, bot, channel) => {
 
 const ping = (txt, bot, channel) => {
   message = "Pong!";
-  sendMessage(message, bot, channel);
+  sendMessage(formatTxt(txt, message), bot, channel);
 };
 
 const pong = (txt, bot, channel) => {
   message = "Ping!";
-  sendMessage(message, bot, channel);
+  sendMessage(formatTxt(txt, message), bot, channel);
 };
 
 const bo = messageSenderGenerator("Ba!");
