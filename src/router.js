74af6da107cbd86a372faaa668a0e97c56bdd813
itzmeowww <winthanasan@gmail.com>
2020-06-27 16:58:58 +0700

    format text again 3

--- src/router.js
+++ src/router.js
@@ -22,10 +22,10 @@ const helpcmd = {
   headers: ["use ! as prefix"],
   type: ["all main commands"],
   commands: [
-    { usage: "version\t\t\t\t\t", desc: "display manually updated version" },
-    { usage: "cf [subcommand]\t", desc: "codeforces commands" },
-    { usage: "dice\t\t\t\t\t", desc: "roll a dice" },
-    { usage: "pick a,b,c,...\t\t\t", desc: "pick one from the list" },
+    { usage: "version\t\t\t\t\t\t", desc: "display manually updated version" },
+    { usage: "cf [subcommand]\t\t", desc: "codeforces commands" },
+    { usage: "dice\t\t\t\t\t\t\t", desc: "roll a dice" },
+    { usage: "pick a,b,c,...\t\t\t\t", desc: "pick one from the list" },
   ],
 };
 
